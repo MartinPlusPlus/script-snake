d547045e32b3c016abf53a00e3463697363b5951
Martin Eldredge <martin.eldredge1@gmail.com>
2020-12-30 02:26:50 -0700

    added color defs and formatting

--- snake.js
+++ snake.js
@@ -1,8 +1,17 @@
 const snakeboard = document.getElementById("gameCanvas");
 const snakeboard_ctx = gameCanvas.getContext("2d");
 
-let snake = [ {x: 200, y: 200}, {x: 190, y: 200},
-    {x: 180, y: 200}, {x: 170, y: 200}, {x: 160, y: 200}
+const board_border = 'black';
+const board_background = "white";
+const snake_col = 'lightblue';
+const snake_border = 'darkblue';
+
+let snake = [
+    {x: 200, y: 200},
+    {x: 190, y: 200},
+    {x: 180, y: 200},
+    {x: 170, y: 200},
+    {x: 160, y: 200}
 ];
 
 function drawSnakePart(snakePart)
