a314a6b94bede00e27fa9af807bcb27df5efd8f7
Martin Eldredge <martin.eldredge1@gmail.com>
2020-12-31 18:47:14 -0700

    remove unecessary code and rename main js file

--- createTag.js
+++
@@ -1,8 +0,0 @@
-// I have no idea why this is here...
-
-document.addEventListener("DOMContentLoaded", function () {
-    pTag = document.querySelector("div");
-    newVal = document.createElement("p");
-    newVal.innerHTML = '';
-    pTag.appendChild(newVal);
-});
