c325de032e89f778790473e8bd9f3ebd7b41884d
Martin Eldredge <martin.eldredge1@gmail.com>
2021-04-04 19:10:21 -0600

    added express server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const router = app.Router;
+const PORT = 3000;
+
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())
+  }
+};
+
+app.use(express.static(__dirname + 'public', options));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`script-snake listening on port ${PORT}`)
+});
